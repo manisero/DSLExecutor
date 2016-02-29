29ccd1ae3f1feb411a0900fec1f00a7956f33083
manisero <michalaniserowicz@gmail.com>
2016-02-29 22:15:43 +0100

    Create DSLExecutor solution

---
+++ dev/Manisero.DSLExecutor/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Manisero.DSLExecutor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Manisero.DSLExecutor")]
+[assembly: AssemblyCopyright("Copyright ©2016, manisero")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d75a6ac-552f-426e-91c0-4b530ca4d66c")]

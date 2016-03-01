249b4ff8f72a351cb70ca0c37c6aed1e0c5c5451
manisero <michalaniserowicz@gmail.com>
2016-03-01 21:26:26 +0100

    Add test project

---
+++ dev/Manisero.DSLExecutor.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Manisero.DSLExecutor.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Manisero.DSLExecutor.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("811e2396-3040-4c67-8c4c-f0c5a0b72cfc")]
---
+++ dev/Manisero.DSLExecutor.Tests/Test.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Manisero.DSLExecutor.Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(true, false);
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void ClassFromOtherProject()
+        {
+            //new ConstantExpressionExecutor()
+        }
+    }
+}

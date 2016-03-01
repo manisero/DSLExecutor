14410a54b1d6fee1e6ef272973d37e1a8fb3685a
manisero <michalaniserowicz@gmail.com>
2016-03-01 22:04:55 +0100

    Add ConstantExpressionExecutorTests

---
+++ dev/Manisero.DSLExecutor.Tests/Runtime/ConstantExpressionExecutorTests.cs
@@ -0,0 +1,32 @@
+﻿using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Manisero.DSLExecutor.Runtime;
+using Xunit;
+
+namespace Manisero.DSLExecutor.Tests.Runtime
+{
+    public class ConstantExpressionExecutorTests
+    {
+        private object Act(IConstantExpression expression)
+        {
+            var executor = new ConstantExpressionExecutor();
+
+            return executor.Execute(expression);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(5)]
+        public void returns_expression_value(int value)
+        {
+            var expression = new ConstantExpression<int>
+                {
+                    Value = value
+                };
+
+            var result = Act(expression);
+
+            Assert.Equal(value, result);
+        }
+    }
+}

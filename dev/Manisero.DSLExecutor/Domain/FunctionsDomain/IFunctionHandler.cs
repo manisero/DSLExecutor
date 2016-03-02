6ed88afe8ac34ef1042e095bf8ed5a90f8be988e
manisero <michalaniserowicz@gmail.com>
2016-03-02 23:36:21 +0100

    Start implementing function execution

---
+++ dev/Manisero.DSLExecutor/Domain/FunctionsDomain/IFunctionHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Manisero.DSLExecutor.Domain.FunctionsDomain
+{
+    public interface IFunctionHandler<TFunction, TResult>
+        where TFunction : IFunction<TResult>
+    {
+        TResult Handle(TFunction function);
+    }
+}
---
+++ dev/Manisero.DSLExecutor/Runtime/FunctionExecution/FunctionHandlerResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Manisero.DSLExecutor.Domain.FunctionsDomain;
+
+namespace Manisero.DSLExecutor.Runtime.FunctionExecution
+{
+    public class FunctionHandlerResolver
+    {
+        public IFunctionHandler<TFunction, TResult> Resolve<TFunction, TResult>()
+            where TFunction : IFunction<TResult>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

de95fcd0b8a369adea5ef50c35de38622e994fe4
manisero <michalaniserowicz@gmail.com>
2016-03-12 20:14:49 +0100

    Reference and use Json.NET

---
+++ dev/Manisero.DSLExecutor.Parser.Json/JsonParser.cs
@@ -0,0 +1,13 @@
+﻿using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Newtonsoft.Json;
+
+namespace Manisero.DSLExecutor.Parser.Json
+{
+    public class JsonParser
+    {
+        public IExpression Parse(string json)
+        {
+            return JsonConvert.DeserializeObject<IExpression>(json);
+        }
+    }
+}

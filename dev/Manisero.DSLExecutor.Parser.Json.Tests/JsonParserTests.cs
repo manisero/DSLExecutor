ccaa246d64fff94f0815bcee65c866ecff39f31f
manisero <michalaniserowicz@gmail.com>
2016-03-12 20:37:32 +0100

    Implement and test parsing ConstantExpression

---
+++ dev/Manisero.DSLExecutor.Parser.Json.Tests/JsonParserTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Manisero.DSLExecutor.Parser.Json.Tests
+{
+    public class JsonParserTests
+    {
+        private IExpression Act(string json)
+        {
+            var parser = new JsonParser();
+
+            return parser.Parse(json);
+        }
+
+        private string Serialize(IExpression expression)
+        {
+            var serializerSettings = new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All
+                };
+
+            return JsonConvert.SerializeObject(expression, serializerSettings);
+        }
+
+        [Fact]
+        public void constant_expression()
+        {
+            var expression = new ConstantExpression<int>
+                {
+                    Value = 3
+                };
+
+            var result = Act(Serialize(expression));
+
+            result.ShouldBeEquivalentTo(expression);
+        }
+    }
+}
--- dev/Manisero.DSLExecutor.Parser.Json/JsonParser.cs
+++ dev/Manisero.DSLExecutor.Parser.Json/JsonParser.cs
@@ -1,13 +1,29 @@
-﻿using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+﻿using System;
+using Manisero.DSLExecutor.Domain.ExpressionsDomain;
 using Newtonsoft.Json;
 
 namespace Manisero.DSLExecutor.Parser.Json
 {
-    public class JsonParser
+    public interface IJsonParser
     {
+        IExpression Parse(string json);
+    }
+
+    public class JsonParser : IJsonParser
+    {
+        private readonly Lazy<JsonSerializerSettings> _serializerSettigns = new Lazy<JsonSerializerSettings>(InitializeSerializerSettigns);
+
         public IExpression Parse(string json)
         {
-            return JsonConvert.DeserializeObject<IExpression>(json);
+            return JsonConvert.DeserializeObject<IExpression>(json, _serializerSettigns.Value);
+        }
+
+        private static JsonSerializerSettings InitializeSerializerSettigns()
+        {
+            return new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All
+                };
         }
     }
 }

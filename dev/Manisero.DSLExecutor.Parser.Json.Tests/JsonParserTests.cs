3306407f7db4ee00cfa7a5dcc10b761604b5db19
manisero <michalaniserowicz@gmail.com>
2016-03-12 20:55:35 +0100

    Test parsing other expressions

--- dev/Manisero.DSLExecutor.Parser.Json.Tests/JsonParserTests.cs
+++ dev/Manisero.DSLExecutor.Parser.Json.Tests/JsonParserTests.cs
@@ -1,5 +1,7 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Manisero.DSLExecutor.Parser.Json.Tests.TestsDomain;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -21,15 +23,82 @@ namespace Manisero.DSLExecutor.Parser.Json.Tests
                     TypeNameHandling = TypeNameHandling.All
                 };
 
-            return JsonConvert.SerializeObject(expression, serializerSettings);
+            return JsonConvert.SerializeObject(expression, Formatting.Indented, serializerSettings);
         }
 
         [Fact]
-        public void constant_expression()
+        public void ConstantExpression()
         {
             var expression = new ConstantExpression<int>
                 {
                     Value = 3
+                };
+
+            var result = Act(Serialize(expression));
+
+            result.ShouldBeEquivalentTo(expression);
+        }
+
+        [Fact]
+        public void FunctionExpression()
+        {
+            var expression = new FunctionExpression<AddFunction, int>
+                {
+                    ArgumentExpressions = new Dictionary<string, IExpression>
+                        {
+                            [nameof(AddFunction.A)] = new ConstantExpression<int> { Value = 3 },
+                            [nameof(AddFunction.B)] = new ConstantExpression<int> { Value = 5 }
+                        }
+                };
+
+            var result = Act(Serialize(expression));
+
+            result.ShouldBeEquivalentTo(expression);
+        }
+
+        [Fact]
+        public void BatchExpression()
+        {
+            var expression = new BatchExpression<string>
+                {
+                    SideExpressions = new IExpression[]
+                        {
+                            new ConstantExpression<int> { Value = 3 },
+                            new ConstantExpression<double> { Value = 5.0 }
+                        },
+                    ResultExpression = new ConstantExpression<string> { Value = "value" }
+                };
+
+            var result = Act(Serialize(expression));
+
+            result.ShouldBeEquivalentTo(expression);
+        }
+
+        [Fact]
+        public void complex_expression()
+        {
+            var expression = new BatchExpression<string>
+                {
+                    SideExpressions = new IExpression[]
+                        {
+                            new FunctionExpression<SubstractFunction, int>
+                                {
+                                    ArgumentExpressions = new Dictionary<string, IExpression>
+                                        {
+                                            [nameof(SubstractFunction.A)] = new FunctionExpression<AddFunction, int>
+                                                {
+                                                    ArgumentExpressions = new Dictionary<string, IExpression>
+                                                        {
+                                                            [nameof(AddFunction.A)] = new ConstantExpression<int> { Value = 1 },
+                                                            [nameof(AddFunction.B)] = new ConstantExpression<int> { Value = 2 }
+                                                        }
+                                                },
+                                            [nameof(SubstractFunction.B)] = new ConstantExpression<int> { Value = 3 }
+                                        }
+                                },
+                            new ConstantExpression<double> { Value = 5.0 }
+                        },
+                    ResultExpression = new ConstantExpression<string> { Value = "value" }
                 };
 
             var result = Act(Serialize(expression));
---
+++ dev/Manisero.DSLExecutor.Parser.Json.Tests/TestsDomain/Functions.cs
@@ -0,0 +1,16 @@
+﻿using Manisero.DSLExecutor.Domain.FunctionsDomain;
+
+namespace Manisero.DSLExecutor.Parser.Json.Tests.TestsDomain
+{
+    public class AddFunction : IFunction<int>
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+    }
+
+    public class SubstractFunction : IFunction<int>
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+    }
+}

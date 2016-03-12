d1382f8e849e214b5931e297b023316f346320f5
manisero <michalaniserowicz@gmail.com>
2016-03-12 19:48:47 +0100

    Cleanup

--- dev/Manisero.DSLExecutor.Tests/DSLExecutorTests/LogStore.cs
+++ dev/Manisero.DSLExecutor.Tests/DSLExecutorTests/LogStore.cs
@@ -5,17 +5,17 @@ namespace Manisero.DSLExecutor.Tests.DSLExecutorTests
 {
     public static class LogStore
     {
-        private static IDictionary<int, IList<string>> _threadLogs = new Dictionary<int, IList<string>>();
+        private static readonly IDictionary<int, IList<string>> ThreadLogs = new Dictionary<int, IList<string>>();
 
         public static IList<string> GetLog()
         {
             var threadId = Thread.CurrentThread.ManagedThreadId;
             IList<string> log;
             
-            if (!_threadLogs.TryGetValue(threadId, out log))
+            if (!ThreadLogs.TryGetValue(threadId, out log))
             {
                 log = new List<string>();
-                _threadLogs.Add(threadId, log);
+                ThreadLogs.Add(threadId, log);
             }
 
             return log;

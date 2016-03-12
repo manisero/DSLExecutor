7405d3d88874195561f412406291b40d2913e293
manisero <michalaniserowicz@gmail.com>
2016-03-12 19:20:50 +0100

    Tested functions execution

--- dev/Manisero.DSLExecutor.Tests/DSLExecutorTests/DSLExecutorTests.cs
+++ dev/Manisero.DSLExecutor.Tests/DSLExecutorTests/DSLExecutorTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
 using Manisero.DSLExecutor.Domain.ExpressionsDomain;
 using Xunit;
 
@@ -8,7 +10,13 @@ namespace Manisero.DSLExecutor.Tests.DSLExecutorTests
     {
         private object Act(IExpression expression)
         {
-            var dslExecutor = new DSLExecutor(null);
+            var functionTypeToHandlerTypeMap = new Dictionary<Type, Type>
+                {
+                    [typeof(AddFunction)] = typeof(AddFunctionHandler),
+                    [typeof(SubstractFunction)] = typeof(SubstractFunctionHandler)
+                };
+
+            var dslExecutor = new DSLExecutor(functionTypeToHandlerTypeMap);
 
             return dslExecutor.ExecuteExpression(expression);
         }
@@ -24,6 +32,53 @@ namespace Manisero.DSLExecutor.Tests.DSLExecutorTests
             var result = Act(expression);
 
             result.Should().Be(expressionValue);
+        }
+
+        [Theory]
+        [InlineData(0, 3)]
+        [InlineData(1, 5)]
+        [InlineData(5, 8)]
+        public void function_expression(int a, int b)
+        {
+            var expression = new FunctionExpression<AddFunction, int>
+                {
+                    ArgumentExpressions = new Dictionary<string, IExpression>
+                        {
+                            [nameof(AddFunction.A)] = new ConstantExpression<int> { Value = a },
+                            [nameof(AddFunction.B)] = new ConstantExpression<int> { Value = b }
+                        }
+                };
+
+            var result = Act(expression);
+
+            result.Should().Be(a + b);
+        }
+
+        [Theory]
+        [InlineData(0, 3, 5)]
+        [InlineData(1, 5, 10)]
+        [InlineData(5, 8, 15)]
+        public void function_expression_nested(int a, int b, int c)
+        {
+            var expression = new FunctionExpression<SubstractFunction, int>
+                {
+                    ArgumentExpressions = new Dictionary<string, IExpression>
+                        {
+                            [nameof(SubstractFunction.A)] = new FunctionExpression<AddFunction, int>
+                                {
+                                    ArgumentExpressions = new Dictionary<string, IExpression>
+                                        {
+                                            [nameof(AddFunction.A)] = new ConstantExpression<int> { Value = a },
+                                            [nameof(AddFunction.B)] = new ConstantExpression<int> { Value = b }
+                                        }
+                                },
+                            [nameof(SubstractFunction.B)] = new ConstantExpression<int> { Value = c }
+                        }
+                };
+
+            var result = Act(expression);
+
+            result.Should().Be(a + b - c);
         }
 
         // TODO: Add more tests
---
+++ dev/Manisero.DSLExecutor.Tests/DSLExecutorTests/Functions.cs
@@ -0,0 +1,38 @@
+﻿using Manisero.DSLExecutor.Domain.FunctionsDomain;
+
+namespace Manisero.DSLExecutor.Tests.DSLExecutorTests
+{
+    // Add
+
+    public class AddFunction : IFunction<int>
+    {
+        public int A { get; set; }
+
+        public int B { get; set; }
+    }
+
+    public class AddFunctionHandler : IFunctionHandler<AddFunction, int>
+    {
+        public int Handle(AddFunction function)
+        {
+            return function.A + function.B;
+        }
+    }
+
+    // Substract
+
+    public class SubstractFunction : IFunction<int>
+    {
+        public int A { get; set; }
+
+        public int B { get; set; }
+    }
+
+    public class SubstractFunctionHandler : IFunctionHandler<SubstractFunction, int>
+    {
+        public int Handle(SubstractFunction function)
+        {
+            return function.A - function.B;
+        }
+    }
+}

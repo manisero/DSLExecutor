6f41e0b7eb0a6dbb7a899ccd36cb8f79c6024485
manisero <michalaniserowicz@gmail.com>
2016-03-12 15:42:33 +0100

    Implement FunctionHandlerResolver

---
+++ dev/Manisero.DSLExecutor/Runtime/FunctionExecution/FunctionHandlerResolution/FunctionHandlerResolvers/ActivatorUsingFunctionHandlerResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Manisero.DSLExecutor.Domain.FunctionsDomain;
+
+namespace Manisero.DSLExecutor.Runtime.FunctionExecution.FunctionHandlerResolution.FunctionHandlerResolvers
+{
+    public class ActivatorUsingFunctionHandlerResolver : IFunctionHandlerResolver
+    {
+        public IFunctionHandler<TFunction, TResult> Resolve<THandler, TFunction, TResult>()
+            where THandler : IFunctionHandler<TFunction, TResult>
+            where TFunction : IFunction<TResult>
+        {
+            return Activator.CreateInstance<THandler>();
+        }
+    }
+}
---
+++ dev/Manisero.DSLExecutor/Runtime/FunctionExecution/FunctionHandlerResolution/IFunctionHandlerResolver.cs
@@ -0,0 +1,11 @@
+﻿using Manisero.DSLExecutor.Domain.FunctionsDomain;
+
+namespace Manisero.DSLExecutor.Runtime.FunctionExecution.FunctionHandlerResolution
+{
+    public interface IFunctionHandlerResolver
+    {
+        IFunctionHandler<TFunction, TResult> Resolve<THandler, TFunction, TResult>()
+            where THandler : IFunctionHandler<TFunction, TResult>
+            where TFunction : IFunction<TResult>;
+    }
+}

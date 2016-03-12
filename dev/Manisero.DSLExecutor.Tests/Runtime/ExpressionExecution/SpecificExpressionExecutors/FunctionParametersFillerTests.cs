054717b283323136bddd0efb300704a167dec6e7
manisero <michalaniserowicz@gmail.com>
2016-03-12 17:36:16 +0100

    Extract FunctionParametersFiller

---
+++ dev/Manisero.DSLExecutor.Tests/Runtime/ExpressionExecution/SpecificExpressionExecutors/FunctionParametersFillerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Manisero.DSLExecutor.Runtime.ExpressionExecution;
+using Manisero.DSLExecutor.Runtime.ExpressionExecution.SpecificExpressionExecutors;
+using Manisero.DSLExecutor.Tests.TestsDomain;
+using NSubstitute;
+using Xunit;
+using FluentAssertions;
+
+namespace Manisero.DSLExecutor.Tests.Runtime.ExpressionExecution.SpecificExpressionExecutors
+{
+    public class FunctionParametersFillerTests
+    {
+        private void Act<TFunction>(TFunction function, IDictionary<string, IExpression> argumentExpressions, IExpressionExecutor expressionExecutor = null)
+        {
+            var filler = new FunctionParametersFiller(new Lazy<IExpressionExecutor>(() => expressionExecutor));
+
+            filler.Fill(function, argumentExpressions);
+        }
+
+        [Theory]
+        [InlineData(0, null)]
+        [InlineData(1, "a")]
+        [InlineData(5, "b")]
+        public void function_with_parameters___passes_function_with_filled_parameters_to_functionExecutor(int argument1Value, string argument2Value)
+        {
+            var function = new FunctionWithParameters();
+            var argumentExpressions = new Dictionary<string, IExpression>
+                {
+                    [nameof(FunctionWithParameters.Parameter1)] = new ConstantExpression<int>(),
+                    [nameof(FunctionWithParameters.Parameter2)] = new ConstantExpression<string>()
+                };
+
+            var expressionExecutor = Substitute.For<IExpressionExecutor>();
+
+            expressionExecutor.Execute(argumentExpressions[nameof(FunctionWithParameters.Parameter1)])
+                              .Returns(argument1Value);
+
+            expressionExecutor.Execute(argumentExpressions[nameof(FunctionWithParameters.Parameter2)])
+                              .Returns(argument2Value);
+
+            Act(function, argumentExpressions, expressionExecutor);
+            
+            function.Parameter1.Should().Be(argument1Value);
+            function.Parameter2.Should().Be(argument2Value);
+        }
+
+        [Fact]
+        public void executes_argumentExpressions_in_order()
+        {
+            var function = new FunctionWithParameters();
+            var argumentExpressions = new Dictionary<string, IExpression>
+                {
+                    [nameof(FunctionWithParameters.Parameter2)] = new ConstantExpression<string>(),
+                    [nameof(FunctionWithParameters.Parameter1)] = new ConstantExpression<int>()
+                };
+
+            var expressionExecutor = Substitute.For<IExpressionExecutor>();
+            var executionOrder = new List<int>();
+
+            expressionExecutor.Execute(argumentExpressions[nameof(FunctionWithParameters.Parameter1)])
+                              .Returns(_ =>
+                                           {
+                                               executionOrder.Add(0);
+                                               return 0;
+                                           });
+
+            expressionExecutor.Execute(argumentExpressions[nameof(FunctionWithParameters.Parameter2)])
+                              .Returns(_ =>
+                                           {
+                                               executionOrder.Add(1);
+                                               return null;
+                                           });
+
+            Act(function, argumentExpressions, expressionExecutor);
+
+            executionOrder.Should().Equal(0, 1);
+        }
+    }
+}
---
+++ dev/Manisero.DSLExecutor/Runtime/ExpressionExecution/SpecificExpressionExecutors/FunctionParametersFiller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+
+namespace Manisero.DSLExecutor.Runtime.ExpressionExecution.SpecificExpressionExecutors
+{
+    public class FunctionParametersFiller
+    {
+        private readonly Lazy<IExpressionExecutor> _expressionExecutorFactory;
+
+        public FunctionParametersFiller(Lazy<IExpressionExecutor> expressionExecutorFactory)
+        {
+            _expressionExecutorFactory = expressionExecutorFactory;
+        }
+
+        public void Fill<TFunction>(TFunction function, IDictionary<string, IExpression> argumentExpressions)
+        {
+            var functionProperties = typeof(TFunction).GetProperties();
+
+            if ((argumentExpressions?.Count ?? 0) != functionProperties.Length)
+            {
+                // TODO: Unit-test this case
+                throw new InvalidOperationException("Arguments number does not match the function's parameters number.");
+            }
+
+            foreach (var property in functionProperties)
+            {
+                FillParameter(function, property, argumentExpressions);
+            }
+        }
+
+        private void FillParameter(object function, PropertyInfo property, IDictionary<string, IExpression> argumentExpressions)
+        {
+            IExpression argumentExpression;
+
+            if (!argumentExpressions.TryGetValue(property.Name, out argumentExpression))
+            {
+                // TODO: Unit-test this case
+                throw new InvalidOperationException($"Argument Expression for '{property.Name}' parameter not found.");
+            }
+
+            if (!property.PropertyType.IsAssignableFrom(argumentExpression.ResultType))
+            {
+                // TODO: Unit-test this case
+                throw new InvalidOperationException($"Result Type of Argument Expression for '{property.Name}' parameter is invalid. Expected: '{property.PropertyType}' or its child. Actual: '{argumentExpression.ResultType}'.");
+            }
+
+            var argument = _expressionExecutorFactory.Value.Execute(argumentExpression);
+
+            if (argument != null)
+            {
+                property.SetValue(function, argument);
+            }
+        }
+    }
+}

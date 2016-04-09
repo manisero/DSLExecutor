c180ed31fed2bfc00fb60489c41a2a5bbb39d381
manisero <michalaniserowicz@gmail.com>
2016-04-10 01:37:52 +0200

    Implemented parsing function call with literal arguments

--- dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/FunctionCallParserTests.cs
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/FunctionCallParserTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
 using Manisero.DSLExecutor.Parser.SampleDSL.Tokens;
 using Sprache;
 using Xunit;
@@ -13,14 +14,24 @@ namespace Manisero.DSLExecutor.Parser.SampleDSL.Tests
         }
 
         [Theory]
-        [InlineData("add(\"1\" \"2\")", "add", new[] { "1", "2" })]
-        public void parses_function_call(string input, string expectedName, string[] expectedArguments)
+        [InlineData("function()", "function")]
+        public void parses_name_and_empty_arguments(string input, string expectedName)
         {
             var result = Act(input);
 
             result.Should().NotBeNull();
             result.FunctionName.Should().Be(expectedName);
-            result.Arguments.ShouldAllBeEquivalentTo(expectedArguments);
+            result.Arguments.Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData("f(\"1\" \"2\")", new[] { "1", "2" })]
+        public void parses_literal_arguments(string input, string[] expectedArgumentValues)
+        {
+            var result = Act(input);
+
+            result.Should().NotBeNull();
+            result.Arguments.Select(x => ((Literal)x).Value).ShouldAllBeEquivalentTo(expectedArgumentValues);
         }
     }
 }

229aad828ff55bf1081107ebb1728c51e6d0b50d
manisero <michalaniserowicz@gmail.com>
2016-04-10 11:23:17 +0200

    Tested FunctionArgumentsParser

--- dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/FunctionArgumentsParserTests.cs
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/FunctionArgumentsParserTests.cs
@@ -15,13 +15,36 @@ namespace Manisero.DSLExecutor.Parser.SampleDSL.Tests
         }
 
         [Theory]
-        [InlineData("\"1\" \"2\"", new[] { "1", "2" })]
-        public void parses_literal_arguments(string input, string[] expectedArgumentValues)
+        [InlineData("\"a\"", "a")]
+        [InlineData("\"1\"", "1")]
+        public void parses_literal_argument(string input, string expectedArgumentValue)
         {
             var result = Act(input);
 
             result.Should().NotBeNull();
-            result.Select(x => ((Literal)x).Value).ShouldAllBeEquivalentTo(expectedArgumentValues);
+            result.Select(x => ((Literal)x).Value).ShouldAllBeEquivalentTo(new[] { expectedArgumentValue });
+        }
+
+        [Theory]
+        [InlineData("f()", "f")]
+        [InlineData("f(\"1\" \"2\")", "f")]
+        public void parses_function_call_argument(string input, string expectedFunctionName)
+        {
+            var result = Act(input);
+
+            result.Should().NotBeNull();
+            result.Select(x => ((FunctionCall)x).FunctionName).ShouldAllBeEquivalentTo(new[] { expectedFunctionName });
+        }
+
+        [Theory]
+        [InlineData("\"1\" f()", "1", "f")]
+        public void parses_multiple_arguments(string input, string expectedArgumentValue, string expectedFunctionName)
+        {
+            var result = Act(input);
+
+            result.Should().NotBeNull();
+            ((Literal)result.ElementAt(0)).Value.Should().Be(expectedArgumentValue);
+            ((FunctionCall)result.ElementAt(1)).FunctionName.Should().Be(expectedFunctionName);
         }
     }
 }

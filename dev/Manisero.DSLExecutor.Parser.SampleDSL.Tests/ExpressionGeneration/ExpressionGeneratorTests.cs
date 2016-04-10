aa3a6bba458420326aade466ae37c4ca638e3980
manisero <michalaniserowicz@gmail.com>
2016-04-10 12:43:52 +0200

    Started implementing ExpressionGenerator

---
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/ExpressionGeneration/ExpressionGeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration;
+using Manisero.DSLExecutor.Parser.SampleDSL.Parsing.Tokens;
+using Xunit;
+
+namespace Manisero.DSLExecutor.Parser.SampleDSL.Tests.ExpressionGeneration
+{
+    public class ExpressionGeneratorTests
+    {
+        private IExpression Act(TokenTree tokenTree)
+        {
+            var generator = new ExpressionGenerator();
+
+            return generator.Generate(tokenTree);
+        }
+
+        [Fact]
+        public void test()
+        {
+            var result = Act(null);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL/ExpressionGeneration/ExpressionGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Manisero.DSLExecutor.Parser.SampleDSL.Parsing.Tokens;
+
+namespace Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration
+{
+    public interface IExpressionGenerator
+    {
+        IExpression Generate(TokenTree tokenTree);
+    }
+
+    public class ExpressionGenerator
+    {
+        public IExpression Generate(TokenTree tokenTree)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

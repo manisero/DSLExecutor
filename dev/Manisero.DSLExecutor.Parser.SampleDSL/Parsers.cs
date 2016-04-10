0b702718a35b91bcf5a3b9773a7c2f1a0035069e
manisero <michalaniserowicz@gmail.com>
2016-04-10 11:07:37 +0200

    Cleanup

--- dev/Manisero.DSLExecutor.Parser.SampleDSL/Parsers.cs
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL/Parsers.cs
@@ -18,9 +18,9 @@ namespace Manisero.DSLExecutor.Parser.SampleDSL
 
         public static readonly Parser<string> FunctionNameParser = Parse.LetterOrDigit.AtLeastOnce().Text().Token();
 
-        public static readonly Lazy<Parser<IEnumerable<IFunctionArgumentToken>>> FunctionArgumentsParser = new Lazy<Parser<IEnumerable<IFunctionArgumentToken>>>(() => LiteralParser.Select(x => (IFunctionArgumentToken)x)
-                                                                                                                                                                                    .Or(FunctionCallParser.Select(x => (IFunctionArgumentToken)x))
-                                                                                                                                                                                    .Many().Token());
+        public static readonly Lazy<Parser<IEnumerable<IFunctionArgumentToken>>> FunctionArgumentsParser = new Lazy<Parser<IEnumerable<IFunctionArgumentToken>>>(() => LiteralParser.Or<IFunctionArgumentToken>(FunctionCallParser)
+                                                                                                                                                                                    .Many()
+                                                                                                                                                                                    .Token());
 
         public static readonly Parser<FunctionCall> FunctionCallParser = (from name in FunctionNameParser
                                                                           from argumentsStart in Parse.Char('(')

fcf256a4c721881f4577e746256c7c9ec60b7b69
manisero <michalaniserowicz@gmail.com>
2016-04-17 10:18:04 +0200

    Cleanup

--- dev/Manisero.DSLExecutor.Parser.SampleDSL/Parsing/Parsers.cs
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL/Parsing/Parsers.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Manisero.DSLExecutor.Parser.SampleDSL.Parsing.Tokens;
 using Sprache;
@@ -31,15 +30,17 @@ namespace Manisero.DSLExecutor.Parser.SampleDSL.Parsing
                                                                     }).Token();
 
         // FunctionCall
-        public static readonly Parser<string> FunctionNameParser = Parse.LetterOrDigit.AtLeastOnce().Text();
+        private const char ArgumentListStart = '(';
+        private const char ArgumentListEnd = ')';
 
+        public static readonly Parser<string> FunctionNameParser = Parse.LetterOrDigit.AtLeastOnce().Text();
         public static readonly Parser<IEnumerable<IFunctionArgumentToken>> FunctionArgumentsParser = LiteralParser.Or<IFunctionArgumentToken>(Parse.Ref(() => FunctionCallParser))
                                                                                                                   .Many();
 
         public static readonly Parser<FunctionCall> FunctionCallParser = (from name in FunctionNameParser
-                                                                          from argumentsStart in Parse.Char('(')
+                                                                          from argumentsStart in Parse.Char(ArgumentListStart)
                                                                           from arguments in Parse.Ref(() => FunctionArgumentsParser)
-                                                                          from argumentsEnd in Parse.Char(')')
+                                                                          from argumentsEnd in Parse.Char(ArgumentListEnd)
                                                                           select new FunctionCall
                                                                               {
                                                                                   FunctionName = name,

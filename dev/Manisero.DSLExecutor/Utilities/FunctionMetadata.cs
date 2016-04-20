18bb2ccd5ecbaf2733d2861236e3faeb592199fd
manisero <michalaniserowicz@gmail.com>
2016-04-20 23:29:51 +0200

    Implemented FunctionMetadataProvider

--- dev/Manisero.DSLExecutor/Utilities/FunctionMetadata.cs
+++ dev/Manisero.DSLExecutor/Utilities/FunctionMetadata.cs
@@ -5,8 +5,6 @@ namespace Manisero.DSLExecutor.Utilities
 {
     public class FunctionMetadata
     {
-        public string Name { get; set; }
-
         public IList<FunctionParameterMetadata> Parameters { get; set; }
 
         public Type ResultType { get; set; }
--- dev/Manisero.DSLExecutor/Utilities/FunctionMetadataProvider.cs
+++ dev/Manisero.DSLExecutor/Utilities/FunctionMetadataProvider.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Manisero.DSLExecutor.Domain.FunctionsDomain;
+using Manisero.DSLExecutor.Extensions;
 
 namespace Manisero.DSLExecutor.Utilities
 {
@@ -11,7 +14,27 @@ namespace Manisero.DSLExecutor.Utilities
     {
         public FunctionMetadata Provide(Type functionType)
         {
-            throw new NotImplementedException();
+            var functionDefinitionImplementation = functionType.GetGenericInterfaceDefinitionImplementation(typeof(IFunction<>));
+
+            if (functionDefinitionImplementation == null)
+            {
+                throw new ArgumentException($"{nameof(functionType)} must implement {typeof(IFunction<>)} interface.",
+                                            nameof(functionType));
+            }
+
+            var parameters = functionType.GetProperties()
+                                         .Select(x => new FunctionParameterMetadata
+                                             {
+                                                 Name = x.Name,
+                                                 Type = x.PropertyType
+                                             })
+                                         .ToList();
+
+            return new FunctionMetadata
+                {
+                    Parameters = parameters,
+                    ResultType = functionDefinitionImplementation.GetGenericArguments()[0]
+                };
         }
     }
 }

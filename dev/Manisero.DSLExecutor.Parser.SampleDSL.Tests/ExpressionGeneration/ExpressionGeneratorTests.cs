e97289525a348a809a456bede4ea4d6b36ca82a1
manisero <michalaniserowicz@gmail.com>
2016-04-20 22:01:42 +0200

    Implemented ExpressionGenerator

--- dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/ExpressionGeneration/ExpressionGeneratorTests.cs
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/ExpressionGeneration/ExpressionGeneratorTests.cs
@@ -1,9 +1,12 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using Manisero.DSLExecutor.Domain.ExpressionsDomain;
-using Manisero.DSLExecutor.Domain.FunctionsDomain;
 using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration;
 using Manisero.DSLExecutor.Parser.SampleDSL.Parsing.Tokens;
 using Manisero.DSLExecutor.Parser.SampleDSL.Tests.TestsDomain;
+using NSubstitute;
 using Xunit;
 
 namespace Manisero.DSLExecutor.Parser.SampleDSL.Tests.ExpressionGeneration
@@ -18,7 +21,7 @@ namespace Manisero.DSLExecutor.Parser.SampleDSL.Tests.ExpressionGeneration
         }
 
         [Fact]
-        public void single_FunctionCall___BatchExpression_with_ResultExpression_only()
+        public void single_FunctionCall___ResultExpression_only()
         {
             var tokenTree = new TokenTree
                 {
@@ -31,12 +34,49 @@ namespace Manisero.DSLExecutor.Parser.SampleDSL.Tests.ExpressionGeneration
                         }
                 };
 
-            var result = Act(tokenTree);
+            var functionExpression = new FunctionExpression<EmptyFunction, Domain.FunctionsDomain.Void>();
+
+            var functionExpressionGenerator = Substitute.For<IFunctionExpressionGenerator>();
+            functionExpressionGenerator.Generate(tokenTree.FunctionCalls[0])
+                                       .Returns(functionExpression);
+
+            var result = Act(tokenTree, functionExpressionGenerator);
 
             result.Should().NotBeNull();
             result.SideExpressions.Should().BeEmpty();
-            result.ResultExpression.As<FunctionExpression<EmptyFunction, Void>>()
-                  .ArgumentExpressions.Should().BeEmpty();
+            result.ResultExpression.Should().BeSameAs(functionExpression);
+        }
+
+        [Fact]
+        public void multiple_FunctionCalls___SideExpressions_and_ResultExpression()
+        {
+            var functionExpressions = new List<Tuple<FunctionCall, IFunctionExpression>>
+                {
+                    new Tuple<FunctionCall, IFunctionExpression>(new FunctionCall { FunctionName = nameof(EmptyFunction) },
+                                                                 new FunctionExpression<EmptyFunction, Domain.FunctionsDomain.Void>()),
+                    new Tuple<FunctionCall, IFunctionExpression>(new FunctionCall { FunctionName = nameof(EmptyFunction) },
+                                                                 new FunctionExpression<EmptyFunction, Domain.FunctionsDomain.Void>()),
+                    new Tuple<FunctionCall, IFunctionExpression>(new FunctionCall { FunctionName = nameof(EmptyFunction) },
+                                                                 new FunctionExpression<EmptyFunction, Domain.FunctionsDomain.Void>())
+                };
+
+            var tokenTree = new TokenTree
+                {
+                    FunctionCalls = functionExpressions.Select(x => x.Item1).ToList()
+                };
+
+            var functionExpressionGenerator = Substitute.For<IFunctionExpressionGenerator>();
+
+            foreach (var expression in functionExpressions)
+            {
+                functionExpressionGenerator.Generate(expression.Item1).Returns(expression.Item2);
+            }
+
+            var result = Act(tokenTree, functionExpressionGenerator);
+
+            result.Should().NotBeNull();
+            result.SideExpressions.ShouldAllBeEquivalentTo(functionExpressions.Take(2).Select(x => x.Item2).ToList());
+            result.ResultExpression.Should().BeSameAs(functionExpressions.Last().Item2);
         }
     }
 }

7708e0f2f8c94422ec197e0d39ddd608d589f0aa
manisero <michalaniserowicz@gmail.com>
2016-05-08 10:17:30 +0200

    Added LiteralParserTests

---
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL.Tests/Parsing/LiteralParsersTests/LiteralParserTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Manisero.DSLExecutor.Parser.SampleDSL.Parsing;
+using Manisero.DSLExecutor.Parser.SampleDSL.Parsing.Tokens;
+using Sprache;
+using Xunit;
+
+namespace Manisero.DSLExecutor.Parser.SampleDSL.Tests.Parsing.LiteralParsersTests
+{
+    public class LiteralParserTests
+    {
+        private Literal Act(string input)
+        {
+            return LiteralParsers.LiteralParser.Parse(input);
+        }
+
+        [Theory]
+        [InlineData("'a'", "a")]
+        public void parses_string(string input, string expectedValue)
+        {
+            var result = Act(input);
+
+            result.Value.Should().Be(expectedValue);
+        }
+    }
+}

dbcd999709c5c5655ed07a28dfa689ea4a8e72ae
manisero <michalaniserowicz@gmail.com>
2016-05-17 23:20:43 +0200

    Add SampleDSLParser

---
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL/ExpressionGeneration/ExpressionGeneratorFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration.BatchExpressionGeneration;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration.ConstantExpressionGeneration;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration.FunctionExpressionGeneration;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration.FunctionExpressionGeneration.ArgumentExpressionsGeneration;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration.FunctionExpressionGeneration.MetadataResolution;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration.FunctionExpressionGeneration.MetadataResolution.FunctionNameToMetadataMapFactories;
+using Manisero.DSLExecutor.Utilities;
+
+namespace Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration
+{
+    public interface IExpressionGeneratorFactory
+    {
+        IExpressionGenerator Create(IEnumerable<Type> functionTypeSamples);
+    }
+
+    public class ExpressionGeneratorFactory : IExpressionGeneratorFactory
+    {
+        public IExpressionGenerator Create(IEnumerable<Type> functionTypeSamples)
+        {
+            IExpressionGenerator expressionGenerator = null;
+
+            expressionGenerator = new ExpressionGenerator(new ConstantExpressionGenerator(),
+                                                          new FunctionExpressionGenerator(new FunctionMetadataResolver(new TypeSamplesAndSuffixConventionBasedFunctionNameToMetadataMapFactory(functionTypeSamples,
+                                                                                                                                                                                               new FunctionContractProvider())),
+                                                                                          new FunctionArgumentExpressionsGenerator(new FunctionArgumentExpressionGenerator(new Lazy<IExpressionGenerator>(() => expressionGenerator)))),
+                                                          new BatchExpressionGenerator(new Lazy<IExpressionGenerator>(() => expressionGenerator)));
+
+            return expressionGenerator;
+        }
+    }
+}
---
+++ dev/Manisero.DSLExecutor.Parser.SampleDSL/SampleDSLParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Manisero.DSLExecutor.Domain.ExpressionsDomain;
+using Manisero.DSLExecutor.Parser.SampleDSL.ExpressionGeneration;
+using Manisero.DSLExecutor.Parser.SampleDSL.Parsing;
+using Sprache;
+
+namespace Manisero.DSLExecutor.Parser.SampleDSL
+{
+    public class SampleDSLParser
+    {
+        private readonly Lazy<IExpressionGenerator> _expressionGenerator;
+
+        public SampleDSLParser(IEnumerable<Type> functionTypeSamples) // TODO: Move functionTypeSamples to some configuration
+        {
+            _expressionGenerator = new Lazy<IExpressionGenerator>(() => InitializeExpressionGenerator(functionTypeSamples));
+        }
+
+        public IExpression Parse(string input)
+        {
+            var tokenTree = TokenTreeParsers.TokenTreeParser.Parse(input);
+
+            return _expressionGenerator.Value.Generate(tokenTree);
+        }
+
+        private IExpressionGenerator InitializeExpressionGenerator(IEnumerable<Type> functionTypeSamples)
+        {
+            return new ExpressionGeneratorFactory().Create(functionTypeSamples);
+        }
+    }
+}

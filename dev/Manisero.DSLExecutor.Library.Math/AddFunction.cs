88070d13168ae6ea8a5e0928b3dcaaa47adc93d5
manisero <michalaniserowicz@gmail.com>
2016-05-18 19:41:22 +0200

    Implement Add and Sub functions

---
+++ dev/Manisero.DSLExecutor.Library.Math/AddFunction.cs
@@ -0,0 +1,19 @@
+﻿using Manisero.DSLExecutor.Domain.FunctionsDomain;
+
+namespace Manisero.DSLExecutor.Library.Math
+{
+    public class AddFunction : IFunction<int>
+    {
+        public int Addend1 { get; set; }
+
+        public int Addend2 { get; set; }
+    }
+
+    public class AddFunctionHandler : IFunctionHandler<AddFunction, int>
+    {
+        public int Handle(AddFunction function)
+        {
+            return function.Addend1 + function.Addend2;
+        }
+    }
+}
---
+++ dev/Manisero.DSLExecutor.Library.Math/SubFunction.cs
@@ -0,0 +1,19 @@
+﻿using Manisero.DSLExecutor.Domain.FunctionsDomain;
+
+namespace Manisero.DSLExecutor.Library.Math
+{
+    public class SubFunction : IFunction<int>
+    {
+        public int Minuend { get; set; }
+
+        public int Subtrahend { get; set; }
+    }
+
+    public class SubFunctionHandler : IFunctionHandler<SubFunction, int>
+    {
+        public int Handle(SubFunction function)
+        {
+            return function.Minuend - function.Subtrahend;
+        }
+    }
+}

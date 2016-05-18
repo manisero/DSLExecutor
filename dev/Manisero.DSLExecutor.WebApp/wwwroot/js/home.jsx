0f9fb73656a002ec14c9606e5651a11ff9eac48d
manisero <michalaniserowicz@gmail.com>
2016-05-18 23:52:46 +0200

    Display output

--- dev/Manisero.DSLExecutor.WebApp/wwwroot/js/home.jsx
+++ dev/Manisero.DSLExecutor.WebApp/wwwroot/js/home.jsx
@@ -1,11 +1,20 @@
 ﻿var Body = React.createClass({
+    getInitialState: function () {
+        return {
+            output: ''  
+        };
+    },
     handleRunClick: function () {
+        var self = this;
+
         $.post('/Home/ProcessDSL',
             {
                 DSL: this.refs.input.value
             },
             function (response) {
-                console.log(response);
+                self.setState({
+                    output: response.Result
+                });
             });
     },
     render: function () {
@@ -20,7 +29,7 @@
                 </div>
                 <div className="col-md-5">
                     <h2>Output</h2>
-                    <div>output...</div>
+                    <div>{this.state.output}</div>
                 </div>
             </div>
 	    );

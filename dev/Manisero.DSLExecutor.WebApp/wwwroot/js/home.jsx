ebbaae44f9ebaaac42b4bbfb22abbe90de73a812
manisero <michalaniserowicz@gmail.com>
2016-05-18 23:30:51 +0200

    Use react.js

---
+++ dev/Manisero.DSLExecutor.WebApp/wwwroot/js/home.jsx
@@ -0,0 +1,24 @@
+﻿var Body = React.createClass({
+    render: function() {
+	    return (
+            <div className="row">
+                <div className="col-md-5">
+                    <h2>Input</h2>
+                    <input type="text" />
+                </div>
+                <div className="col-md-2">
+                    <input type="button" value="Run" />
+                </div>
+                <div className="col-md-5">
+                    <h2>Output</h2>
+                    <div>output...</div>
+                </div>
+            </div>
+	    );
+    }
+});
+
+ReactDOM.render(
+    <Body />,
+    document.getElementById('body')
+);
